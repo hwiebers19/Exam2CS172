19fc1d7f0c671c08eeacd49ccda1e5a1770eb2e7
Heidi Wiebers <hwiebers19@hotmail.com>
2016-11-10 12:47:43 -0800

    initial

---
+++ Exam2CS172/Exam2CS172/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Exam2CS172
+//
+//  Created by Heidi Wiebers on 11/10/16.
+//  Copyright © 2016 Heidi Wiebers. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
